aafa82eaca655b41882c36e3bcb9133144d813f0
Annie Luc <annieluc@google.com>
2025-07-31 14:38:37 -0700

    feat: Add c8 dependency and 'coverage-report' script to report on code coverage
    PiperOrigin-RevId: 789476954

--- test/generate_report.sh
+++ test/generate_report.sh
@@ -1,19 +1,16 @@
+#!/usr/bin/env bash
+
 #  @license
 #  Copyright 2025 Google LLC
 #  SPDX-License-Identifier: Apache-2.0
 
-#!/bin/bash
-set -x
-# The directory of the script.
-DIR=$(dirname "${BASH_SOURCE[0]}")
+set -ex
 
-# The temp directory used, within $DIR omit the -p parameter to create a
-# temporary directory in the default location
-WORK_DIR=`mktemp -d -p "$DIR"`
+WORK_DIR="$(mktemp -d)"
 DEFAULT_NYC_OUTPUT_DIR="${PWD}/.nyc_output/"
 
 # Check if tmp dir was created.
-if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+if [ ! -d "$WORK_DIR" ]; then
   echo "Could not create temp dir"
   exit 1
 fi
@@ -31,19 +28,19 @@ trap cleanup EXIT
 
 
 UNIT=coverage-unit-test
-TABLE=coverage-table-test
-
-# TODO(b/398045499): Add live tests back to the coverage report.
-ALL_TESTS_IN_UNIT_EXCEPT_LIVE_TESTS=$(find test/unit/ -type f -name "*_test.ts" ! -name "live_test.ts" )
+SYSTEM=coverage-system-test
+# TODO: b/435204110 - Add coverage for table tests.
 
 # Generate the reports for each test suite separately to avoid covering each
 # other.
-nyc --reporter=json --report-dir=./${WORK_DIR}/${UNIT} --require ts-node/register jasmine dist/test/unit/**/*_test.js ${ALL_TESTS_IN_UNIT_EXCEPT_LIVE_TESTS}
-nyc --reporter=json --report-dir=./${WORK_DIR}/${TABLE} --require ts-node/register jasmine test/g3/table_test.ts
+tsc
+GOOGLE_API_KEY=googapikey GOOGLE_CLOUD_PROJECT=googcloudproj GOOGLE_CLOUD_LOCATION=googcloudloc
+c8 --reporter=json --report-dir=./${WORK_DIR}/${UNIT} jasmine dist/test/unit/**/*_test.js dist/test/unit/*_test.js
+c8 --reporter=json --report-dir=./${WORK_DIR}/${SYSTEM} jasmine dist/test/system/node/*_test.js -- --test-server
 
 # Move all the generated coverage reports to the same directory to merge reports.
 mv ./${WORK_DIR}/${UNIT}/coverage-final.json  ./${WORK_DIR}/${UNIT}-coverage-report.json
-mv ./${WORK_DIR}/${TABLE}/coverage-final.json  ./${WORK_DIR}/${TABLE}-coverage-report.json
+mv ./${WORK_DIR}/${SYSTEM}/coverage-final.json  ./${WORK_DIR}/${SYSTEM}-coverage-report.json
 
 # Clean up the directory to avoid contamination, nyc will generate this
 # directory everytime.
@@ -55,3 +52,5 @@ nyc merge ./${WORK_DIR} --output-file=${DEFAULT_NYC_OUTPUT_DIR}/coverage-report.
 # Convert and present the merged report in ./.nyc_output
 nyc report --reporter=text --reporter=lcov --report-dir=${DEFAULT_NYC_OUTPUT_DIR}
 
+# Check coverage is above threshold.
+nyc check-coverage --lines 50 --statements 50 --functions 40 --branches 75
